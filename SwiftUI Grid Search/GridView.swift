b38a8d2ad509e49163ac57b364833b308a63ee33
Matheus Timbó <matheusfild4@hotmail.com>
2021-01-20 12:37:10 -0300

    first commit

---
+++ SwiftUI Grid Search/GridView.swift
@@ -0,0 +1,96 @@
+//
+//  ContentView.swift
+//  SwiftUI Grid Search
+//
+//  Created by Matheus Timbó on 20/01/21.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct RSS: Decodable {
+    let feed: Feed
+    
+}
+
+struct Feed: Decodable {
+    let results: [Result]
+}
+
+struct Result: Decodable, Hashable {
+    let copyright, name, artworkUrl100, releaseDate: String
+}
+
+class GridViewModel: ObservableObject {
+    @Published var items = 0..<5
+    
+    @Published var results = [Result]()
+    
+    init(){
+        Timer.scheduledTimer(withTimeInterval: 2, repeats: false) {
+            (_) in self.items = 0..<15
+        }
+        
+        guard let url = URL(string: "https://rss.itunes.apple.com/api/v1/us/ios-apps/new-apps-we-love/all/100/explicit.json") else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            guard let data = data else {return }
+            do {
+                let rss = try JSONDecoder().decode(RSS.self, from: data)
+                self.results = rss.feed.results
+            } catch {
+                print("Failed to decode: \(error)")
+            }
+        }.resume()
+        
+    }
+}
+
+struct GridView: View {
+    
+    @ObservedObject var vm = GridViewModel()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView{
+                LazyVGrid(columns: [
+                    GridItem(.flexible(minimum: 50, maximum: 200), spacing: 16, alignment: .top),
+                    GridItem(.flexible(minimum: 50, maximum: 200), spacing: 16, alignment: .top),
+                    GridItem(.flexible(minimum: 50, maximum: 200), spacing: 16, alignment: .top)
+                ], alignment: .leading, spacing: 16, content: {
+                    ForEach(vm.results, id: \.self) { app in
+                        AppInfo(app: app)
+                    }
+                    
+                }).padding(.horizontal, 12)
+            }.navigationTitle("Grid Search")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridView()
+    }
+}
+
+struct AppInfo: View {
+    let app: Result
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            KFImage(URL(string: app.artworkUrl100))
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(22)
+            
+            Text(app.name)
+                .font(.system(size: 10, weight: .semibold))
+                .padding(.top, 4    )
+            Text(app.releaseDate)
+                .font(.system(size: 9, weight: .regular))
+            Text(app.copyright)
+                .font(.system(size: 9, weight: .regular))
+                .foregroundColor(.gray)
+        }
+    }
+}
---
+++ SwiftUI Grid Search/SwiftUI_Grid_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Grid_SearchApp.swift
+//  SwiftUI Grid Search
+//
+//  Created by Matheus Timbó on 20/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Grid_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GridView()
+        }
+    }
+}
